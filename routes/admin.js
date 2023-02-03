396869b0661d5671339e1b1b03dff1adc0d72c04
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-02-03 13:47:34 +0530

    product model created

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,18 @@
-const path=require('path')
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir=require('../util/path')
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
-//     /admin/add-product  
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir,'views','add-product.html'))
-});
 
-//    admin/add-product
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
