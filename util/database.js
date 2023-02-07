24ba6adf68823052f8ba25e2d2b3a1f7c1476f9f
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2023-02-07 17:10:08 +0530

    implementing sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node_complete", "root", "Amitbhand68@", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node_complete",
-  password: "Amitbhand68@",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
